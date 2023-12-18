11ac5ac737d980849646d6ecfa915fe6531d65fb
Jinny <jinny9650@gmail.com>
2023-12-18 18:40:49 +0900

    complete-todo-list

--- JavaScript/JAVASCRIPT_COURSE/scripts/11-todo-list.js
+++ JavaScript/JAVASCRIPT_COURSE/scripts/11-todo-list.js
@@ -6,21 +6,34 @@ function renderTodoList() {
 	let todoListHTML = '';
 
 	for(let i=0; i<todoList.length; ++i) {
-		const todo = todoList[i];
-		const html = `<p>${todo}</p>`;
+		const todoObject = todoList[i];
+    const { task, date } = todoObject;
+		const html = `
+      <div>${task}</div> 
+      <div>${date}</div>
+      <button class= "delete-button" onclick ="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      ">Delete</button>`;
+		
 		todoListHTML += html;
 	}
 
 	document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
 
+
 function addTodo() {
 	const inputElement = document.querySelector('.js-list-input');
+  const inputDate = document.querySelector('.js-date-input');
 	// inputELement를 .value로 받아서 inputElement = ''로 초기화하면 초기화가 안됨, .value는 call by value라서 주소안에 진짜 값은 바꾸지못함
-	todoList.push(inputElement.value);
-	renderTodoList();
+  if(inputElement.value != '') {
+    todoList.push({task: inputElement.value, 
+      date: inputDate.value });
+    renderTodoList();
 
-	inputElement.value = '';
+    inputElement.value = '';
+  }
 }
 
 
